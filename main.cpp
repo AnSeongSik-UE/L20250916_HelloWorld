07defbe6ff3acdd3eadf66afee21efaec8fdf1ce
AnSeongSik-unreal <ahnbs135@naver.com>
2025-09-16 14:46:24 +0900

    아이템 선택
    아이템 선택

--- main.cpp
+++ main.cpp
@@ -6,76 +6,26 @@ using namespace std;
 
 int main()
 {
-    int PlayerX = 0;
-    int PlayerY = 0;
-    bool bIsRunning = true;
-    char PlayerShape = 'P';
+	int item = 3;
 
-    while(bIsRunning)
-    {
-        int KeyCode = _getch();
+	cin >> item;
 
-        if (KeyCode == 'w')
-        {
-            PlayerY--;
-        }
-        else if (KeyCode == 's')
-        {
-            PlayerY++;
-        }
-        else if (KeyCode == 'a')
-        {
-            PlayerX--;
-        }
-        else if (KeyCode == 'd')
-        {
-            PlayerX++;
-        }
-        else if (KeyCode == 'q')
-        {
-            bIsRunning = false;
-        }
-        system("cls");
-
-        COORD Cur;
-        Cur.X = PlayerX;
-        Cur.Y = PlayerY;
-        SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), Cur);
-
-        cout << PlayerShape << endl;
-    }
-
-    //PlayerX++;
-    //PlayerY++;
-
-    //COORD Cur;
-    //Cur.X = PlayerX;
-    //Cur.Y = PlayerY;
-    //SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), Cur);
-
-    //cout << PlayerShape << endl;
-    //_getch();
-    //system("cls");
-
-    //PlayerX++;
-
-    //Cur.X = PlayerX;
-    //Cur.Y = PlayerY;
-    //SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), Cur);
-
-    //cout << PlayerShape << endl;
-    //_getch();
-    //system("cls");
-
-    //PlayerY++;
-
-    //Cur.X = PlayerX;
-    //Cur.Y = PlayerY;
-    //SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), Cur);
-
-    //cout << PlayerShape << endl;
-    //_getch();
-    //system("cls");
+	if (item == 1)
+	{
+		cout << "ÃÑ" << endl;
+	}
+	else if (item == 2)
+	{
+		cout << "Ä®" << endl;
+	}
+	else if (item == 3)
+	{
+		cout << "ÇÁ¶óÀÌÆÒ" << endl;
+	}
+	else
+	{
+		cout << "ÁÖ¸Ô" << endl;
+	}
 
 	return 0;
 }
